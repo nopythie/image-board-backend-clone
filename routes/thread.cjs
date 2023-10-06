2ac8ed2f1e57cdf67ec3afc7049a35325cfeeb09
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-06 16:54:29 +0200

    fix storage

--- routes/thread.cjs
+++ routes/thread.cjs
@@ -24,6 +24,26 @@ const upload = multer({
   }),
 });
 
+const listObjects = async (bucketName) => {
+  const params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+  };
+  const command = new ListObjectsCommand(params);
+  try {
+    const data = await s3.send(command);
+    console.log("Objets dans le bucket:");
+    data.Contents.forEach((object) => {
+      console.log(object.Key);
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// Utilisation
+listObjects("nom-de-votre-bucket");
+
 // GET every threads
 router.get("/", getThreads);
 
