1f2dd687789a714b1d8333b4f3d212a12d1e5fc6
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-06 12:13:55 +0200

    fix storage

--- routes/thread.cjs
+++ routes/thread.cjs
@@ -7,14 +7,7 @@ const {
   createThread,
   createReply,
 } = require("../controllers/threadController.cjs");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // GET every threads
