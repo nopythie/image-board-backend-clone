9e575c1536706cc8cc095b7a9b9ce495583cfe19
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-09 10:09:53 +0200

    fix storage

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -83,6 +83,7 @@ const createThread = async (req, res) => {
 
   // Valider le type d'image
   const result = await validateImageType(imageBuffer);
+  console.log(result);
   if (!result.ok) {
     console.error(result.error);
     return res.status(400).json({ error: "Invalid file format." });
