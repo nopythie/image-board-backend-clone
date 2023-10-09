360a62fa44f4dc24017ac68c0c788e57a1473898
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-09 09:57:19 +0200

    fix storage

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express, { json } from "express";
 import "dotenv/config";
 import { connect } from "mongoose";
 import threadRoutes from "./routes/thread.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import { join } from "path";
 import cors from "cors";
 const corsOptions = {
