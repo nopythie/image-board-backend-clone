3e7fc0b1e406224ac9988a3a40ada1e911a951b1
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-09 09:15:50 +0200

    fix storage

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const compression = require("compression");
-const express = require("express");
+import compression from "compression";
+import express, { json } from "express";
 require("dotenv").config();
-const mongoose = require("mongoose");
-const threadRoutes = require("./routes/thread.cjs");
-const path = require("path");
-const cors = require("cors");
+import { connect } from "mongoose";
+import threadRoutes from "./routes/thread.cjs";
+import { join } from "path";
+import cors from "cors";
 const corsOptions = {
   origin: "https://cemus.github.io",
   credentials: true,
@@ -15,16 +15,15 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors(corsOptions));
 app.use(compression());
-app.use(express.json());
+app.use(json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
-app.use(express.static(path.join(__dirname, "images")));
+app.use(express.static(join(__dirname, "images")));
 app.use("/api/threads", threadRoutes);
 
-mongoose
-  .connect(process.env.DB_URI)
+connect(process.env.DB_URI)
   .then(() => {
     app.listen(port, () => {
       console.log(`Serveur lancé.`);
